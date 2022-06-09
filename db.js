a193d66b405ffd8161b1502bb47975b0da2878e1
huydoan <dthanhhuy132@gmail.com>
2022-06-09 09:36:19 +0700

    time 1

---
+++ db.js
@@ -0,0 +1,13 @@
+const comments = require("./data/comments.json");
+const fastPosts = require("./data/fastPosts.json");
+const notifications = require("./data/notifications.json");
+const posts = require("./data/posts.json");
+const users = require("./data/users.json");
+
+module.exports = {
+  comments,
+  fastPosts: fastPosts,
+  notifications: notifications,
+  posts: posts,
+  users: users,
+};
