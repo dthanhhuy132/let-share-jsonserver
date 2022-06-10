3c08e363c1034d20dbe2273f496f2b8088a8891e
huydoan <dthanhhuy132@gmail.com>
2022-06-10 15:22:00 +0700

    update category

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const fastPosts = require("./data/fastPosts.json");
 const notifications = require("./data/notifications.json");
 const posts = require("./data/posts.json");
 const users = require("./data/users.json");
+const categories = require("./data/categories.json");
 
 module.exports = {
   comments,
@@ -10,4 +11,5 @@ module.exports = {
   notifications: notifications,
   posts: posts,
   users: users,
+  categories: categories,
 };
